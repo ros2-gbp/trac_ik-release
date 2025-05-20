142054b09984a2406459a3db18ac2b189ed329bd
Kenji Brameld <kbrameld@traclabs.com>
2025-05-20 13:39:08 +0000

    Rebase from 'upstream'

--- trac_ik_kinematics_plugin/include/trac_ik/trac_ik_kinematics_plugin.hpp
+++ trac_ik_kinematics_plugin/include/trac_ik/trac_ik_kinematics_plugin.hpp
@@ -31,8 +31,8 @@ OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef TRAC_IK_KINEMATICS_PLUGIN_
 #define TRAC_IK_KINEMATICS_PLUGIN_
 
-#include <moveit/kinematics_base/kinematics_base.h>
-#include <moveit/robot_model/robot_model.h>
+#include <moveit/kinematics_base/kinematics_base.hpp>
+#include <moveit/robot_model/robot_model.hpp>
 #include <kdl/chain.hpp>
 #include <kdl/jntarray.hpp>
 
@@ -202,6 +202,8 @@ private:
 
   std::shared_ptr<trac_ik_kinematics::ParamListener> param_listener_;
   std::shared_ptr<trac_ik_kinematics::Params> params_;
+
+  std::shared_ptr<random_numbers::RandomNumberGenerator> rng_;
 
 }; // end class
 }
